4e73a72c285d9722840cc9f513e0e1283b844672
remimal <remi.malleville@eleve.ensai.fr>
2024-01-15 15:50:28 +0100

    modif commentaires

--- main.py
+++ main.py
@@ -1,7 +1,21 @@
 if __name__=="__main__":
     from datetime import datetime
+    import pytz
 
-    now = datetime.now()
+    # Get the timezone object for Reunion
+    tz_Reunion = pytz.timezone('Indian/Reunion') 
 
-    current_time = now.strftime("%H:%M:%S")
-    print("Current Time =", current_time)
+    # Get the current time in Reunion
+    datetime_Reunion = datetime.now(tz_Reunion)
+
+    # Format the time as a string and print it
+    print("Reunion time:", datetime_Reunion.strftime("%H:%M:%S"))
+
+    # Get the timezone object for Paris
+    tz_Paris = pytz.timezone('Europe/Paris')
+
+    # Get the current time in Paris
+    datetime_Paris = datetime.now(tz_Paris)
+
+    # Format the time as a string and print it
+    print("Paris time:", datetime_Paris.strftime("%H:%M:%S"))
