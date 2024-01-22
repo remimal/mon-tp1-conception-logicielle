f5945fa3d293e98cab7f35ba8084206e0f1f91e8
remimal <rmalleville@gmail.com>
2024-01-22 16:20:01 +0100

    modif main.py pour log

--- main.py
+++ main.py
@@ -1,21 +1,22 @@
+from datetime import datetime
+import pytz
+import logging
+
+def get_time(timezone):
+    if timezone is None:
+        logging.error("aucune timezone n'a été renseignée")
+        raise ValueError("aucune timezone n'a été renseignée")
+    tz=pytz.timezone(timezone) 
+    logging.debug(f"Demande d'heure sur le timezone : {tz}")
+    datetime_tz=datetime.now(tz)
+    return "Heure de " + timezone + " " + datetime_tz.strftime("%H:%M:%S")
+
 if __name__=="__main__":
-    from datetime import datetime
-    import pytz
 
-    # Get the timezone object for Reunion
-    tz_Reunion = pytz.timezone('Indian/Reunion') 
+    logging.basicConfig(filename=".log", encoding='utf-8', level=logging.DEBUG)
+    logging.info(f"Lancement du traitement")
 
-    # Get the current time in Reunion
-    datetime_Reunion = datetime.now(tz_Reunion)
+    print(get_time('Indian/Reunion'))
+    print(get_time('Europe/Paris'))
+    print(get_time(None))
 
-    # Format the time as a string and print it
-    print("Reunion time:", datetime_Reunion.strftime("%H:%M:%S"))
-
-    # Get the timezone object for Paris
-    tz_Paris = pytz.timezone('Europe/Paris')
-
-    # Get the current time in Paris
-    datetime_Paris = datetime.now(tz_Paris)
-
-    # Format the time as a string and print it
-    print("Paris time:", datetime_Paris.strftime("%H:%M:%S"))
