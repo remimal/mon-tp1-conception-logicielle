47edfbe9cd2b0a87757ef28b8e80ff9f63ea42bc
remimal <rmalleville@gmail.com>
2024-02-12 16:11:04 +0100

    fastapi

--- main.py
+++ main.py
@@ -32,21 +32,41 @@ def get_time(timezone):
     return "Heure de " + timezone + " " + datetime_tz.strftime("%H:%M:%S")
 
 
-def get_product(barcode):
+def get_product(barcode: str)-> dict:
     url=f"https://world.openfoodfacts.org/api/v0/product/{barcode}.json"
+    logging.info(f"recherche du produit {barcode}")
     response = requests.get(url)
     data=response.json()
     return data
 
 
-if __name__=="__main__":
+# s'execute avec :
+# uvicorn main:app --reload
+from typing import Union
 
-    logging.basicConfig(filename=get_fichier_sortie(), encoding='utf-8', level=logging.DEBUG)
-    logging.info(f"Lancement du traitement")
+from fastapi import FastAPI
 
-    print(get_time('Indian/Reunion'))
-    print(get_time('Europe/Paris'))
+app = FastAPI()
+
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: Union[str, None] = None):
+    return {"item_id": item_id, "q": q}
+
+# if __name__=="__main__":
+
+    # logging.basicConfig(filename=get_fichier_sortie(), encoding='utf-8', level=logging.DEBUG)
+    # logging.info(f"Lancement du traitement")
+
+    # print(get_time('Indian/Reunion'))
+    # print(get_time('Europe/Paris'))
     # print(get_time(None))
 
-    print(get_product("8000500273296"))
+    # print(get_product("8000500273296"))
 
+   
